1c3570e4368331c1ef02097b4d84c551ba44e801
Melanie Simet <melanie.simet@gmail.com>
2019-11-18 15:07:53 -0600

    Add code & tests to make combinations of indices & joint appearance matrices

--- study_groups/group_generator.py
+++ study_groups/group_generator.py
@@ -17,12 +17,102 @@ class GroupGenerator:
         self.matrix = np.zeros((self.n, self.n))
         if 'seed' in config:
             np.random.seed(config['seed'])
-        
-    
+
+    def pairs(self, divisions):
+        """
+            Return a set of pairs of joint appearances in a set of divisions.  For example, if the
+            divisions are `[[0, 1, 4], [2, 3, 5]]`, then return a list of the pairs that appear
+            together, in both permutations:
+            ```
+                [0, 1], [1, 0], [0, 4], [4, 0], [1, 4], [4, 1],
+                [2, 3], [3, 2], [2, 5], [5, 2], [3, 5], [5, 3].
+            ```
+            which is useful for indexing into a numpy array.
+
+            Parameters:
+            -----------
+            divisions: list of iterables
+                A set of small groups whose elements collectively are the set of self.indices
+            Returns:
+            --------
+                A list of lists as described above.
+        """
+        return_list = []
+        for division in divisions:
+            for i, element1 in enumerate(division):
+                for element2 in division[i+1:]:
+                    return_list.append([element1, element2])
+                    return_list.append([element2, element1])
+        return return_list
+
+    def pair_matrix(self, divisions):
+        """
+            Return a pair matrix representing joint appearances in the subgroups defined by
+            `divisions`. For example, if the divisions are `[[0, 1, 4], [2, 3, 5]]`, then return
+            ```
+            [[0 1 0 0 1 0]
+             [1 0 0 0 1 0]
+             [0 0 0 1 0 1]
+             [0 0 1 0 0 1]
+             [1 1 0 0 0 0]
+             [0 0 1 1 0 0]]
+            ```
+            Note that all diagonal elements are always 0, just for convenience (we know things must
+            appear with themselves).
+
+            Parameters:
+            -----------
+                divisions: list of iterables
+                    A set of small groups whose elements collectively are the set of self.indices
+            Returns:
+            --------
+                pair_matrix: a np.array of size (len(self.indices), len(self.indices)) indicating
+                joint appearances
+        """
+        matrix = np.zeros((self.n, self.n))
+        pairs = self.pairs(divisions)
+        pairs = ([p[0] for p in pairs], [p[1] for p in pairs])
+        matrix[pairs] += 1
+        return matrix
+
+    def _generate_combinations(self, n, i=0):
+        """ Produce all combinations from self.indices[i:] that have length n. """
+        if n == 1:
+            return [[ii] for ii in self.indices[i:]]
+        return_list = []
+        for ii in range(i, self.n):
+            return_list.extend([[ii] + g for g in self._generate_combinations(n-1, ii+1)])
+        return return_list
+
     def generate_combinations(self, n):
-        # Produce a list of all possible combinations of size n from our internal set of students.
-        pass
-        
+        """
+            Produce all possible combinations from the list of `self.indices` with length `n`, and
+            return a list of tuples where the first item is the combinations and the second is the
+            matrix of indices appearing together.  For example, if `self.indices = [0, 1, 2, 3]`,
+            then calling this with n=2 will return
+            [((0, 1, 2), [[0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]]),
+             ((0, 1, 3), [[0, 1, 0, 1], [1, 0, 0, 1], [0, 0, 0, 0], [1, 1, 0, 0]]),
+             ((0, 2, 3), [[0, 0, 1, 1], [0, 0, 0, 0], [1, 0, 0, 1], [1, 0, 1, 0]]),
+             ((1, 2, 3), [[0, 0, 0, 0], [0, 0, 1, 1], [0, 1, 0, 1], [0, 1, 1, 0]])],
+            not necessarily in that order.
+
+            Parameters:
+            -----------
+                n : int
+                    The number of items in the individual group
+            Returns:
+            --------
+                return_list: a list of tuples where the first item of the tuple is a combination and
+                             the second is a matrix describing which items appear together.
+        """
+        if n >= self.n:
+            raise ValueError(
+                "Requested number of items must be less than or equal to the number of 'names'")
+        if n < 0:
+            raise ValueError("Requested number of items must be positive")
+        return_list = self._generate_combinations(n)
+        return [(ret, self.pair_matrix([ret])) for ret in return_list]
+
     def generate_divisions(self, dist):
         # Generate all possible ways to divide our internal set of students into a distribution of groups dist.
         # For example, if dist=[4,4,4] for 12 students, take the results of generate_combinations(4) and
--- tests/test_group_generator.py
+++ tests/test_group_generator.py
@@ -38,7 +38,149 @@ class TestInitialization(unittest.TestCase):
         np.random.seed(271)
         ri_truth = np.random.rand()
         np.testing.assert_equal(ri, ri_truth)
-        
-    
-if __name__=='__main__':
-    test_init()
+
+class TestMatrices(unittest.TestCase):
+    """ Tests that joint appearance matrices can be created from input lists of indices. """
+    def setUp(self):
+        """ Initialize a group generator to test against """
+        self.gg = sg.GroupGenerator('test3.yaml')
+
+    def test_pairs(self):
+        """ Check that all joint-appearance pairs are correctly obtained """
+        input1 = [[0, 1], [2, 3], [4, 5]]
+        output1 = [[0, 1], [1, 0], [2, 3], [3, 2], [4, 5], [5, 4]]
+        result1 = self.gg.pairs(input1)
+        np.testing.assert_equal(result1, output1)
+
+        input2 = [[0, 1, 2], [3, 4, 5]]
+        output2 = [[0, 1], [1, 0], [0, 2], [2, 0], [1, 2], [2, 1],
+                   [3, 4], [4, 3], [3, 5], [5, 3], [4, 5], [5, 4]]
+        result2 = self.gg.pairs(input2)
+        np.testing.assert_equal(result2, output2)
+
+        input3 = ((0, 1, 2, 3, 4, 5),) # make sure this works with other iterables
+        output3 = [[0, 1], [1, 0], [0, 2], [2, 0], [0, 3], [3, 0], [0, 4], [4, 0], [0, 5], [5, 0],
+                   [1, 2], [2, 1], [1, 3], [3, 1], [1, 4], [4, 1], [1, 5], [5, 1], [2, 3], [3, 2],
+                   [2, 4], [4, 2], [2, 5], [5, 2], [3, 4], [4, 3], [3, 5], [5, 3], [4, 5], [5, 4]]
+        result3 = self.gg.pairs(input3)
+        np.testing.assert_equal(result3, output3)
+
+        input4 = [[0], [1, 2], [3, 4, 5]]
+        output4 = [[1, 2], [2, 1], [3, 4], [4, 3], [3, 5], [5, 3], [4, 5], [5, 4]]
+        result4 = self.gg.pairs(input4)
+        np.testing.assert_equal(result4, output4)
+
+    def test_pair_matrix(self):
+        """ Test that joint appearances are correctly registered in a pair matrix """
+        input1 = [[0, 1], [2, 3], [4, 5]]
+        output1 = np.array([[0, 1, 0, 0, 0, 0],
+                            [1, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 0, 0],
+                            [0, 0, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 1],
+                            [0, 0, 0, 0, 1, 0]])
+        result1 = self.gg.pair_matrix(input1)
+        np.testing.assert_equal(result1, output1)
+
+        input2 = [[0, 1, 2], [3, 4, 5]]
+        output2 = np.array([[0, 1, 1, 0, 0, 0],
+                            [1, 0, 1, 0, 0, 0],
+                            [1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1],
+                            [0, 0, 0, 1, 0, 1],
+                            [0, 0, 0, 1, 1, 0]])
+        result2 = self.gg.pair_matrix(input2)
+        np.testing.assert_equal(result2, output2)
+
+        input3 = ((0, 1, 2, 3, 4, 5),) # make sure this works with other iterables
+        output3 = np.array([[0, 1, 1, 1, 1, 1],
+                            [1, 0, 1, 1, 1, 1],
+                            [1, 1, 0, 1, 1, 1],
+                            [1, 1, 1, 0, 1, 1],
+                            [1, 1, 1, 1, 0, 1],
+                            [1, 1, 1, 1, 1, 0]])
+        result3 = self.gg.pair_matrix(input3)
+        np.testing.assert_equal(result3, output3)
+
+        input4 = [[0], [1, 2], [3, 4, 5]]
+        output4 = np.array([[0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1],
+                            [0, 0, 0, 1, 0, 1],
+                            [0, 0, 0, 1, 1, 0]])
+        result4 = self.gg.pair_matrix(input4)
+        np.testing.assert_equal(result4, output4)
+
+class TestCombinations(unittest.TestCase):
+    """ Tests that combinations of indices and corresponding pair matrices can be
+        correctly created.
+    """
+    def setUp(self):
+        """ Initialize a group generator to test against """
+        self.gg = sg.GroupGenerator('test3.yaml')
+
+    def add_pair_matrix(self, ret):
+        """ Add a pair matrix for generate_combinations comparisons """
+        return [(r, self.gg.pair_matrix([r])) for r in ret]
+
+    def test_internal_combinations(self):
+        """ Test the internal code that generates combinations of indices """
+        results = self.gg._generate_combinations(1, i=5)
+        output = [[5]]
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(1, i=3)
+        output = [[3], [4], [5]]
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(2, i=3)
+        output = [[3, 4], [3, 5], [4, 5]]
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(2, i=0)
+        output = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5],
+                  [1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
+                  [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(4, i=3)
+        output = []
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(3, i=1)
+        output = [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5],
+                  [1, 4, 5], [2, 3, 4], [2, 3, 5], [2, 4, 5], [3, 4, 5]]
+        np.testing.assert_equal(results, output)
+
+        results = self.gg._generate_combinations(6)
+        output = [[0, 1, 2, 3, 4, 5]]
+        np.testing.assert_equal(results, output)
+
+    def test_combinations(self):
+        """ Test that the full set of combinations is generated for each length """
+        np.testing.assert_raises(ValueError, self.gg.generate_combinations, 7)
+
+        results = self.gg.generate_combinations(2)
+        output = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5],
+                  [1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
+                  [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+        np.testing.assert_equal(results, self.add_pair_matrix(output))
+
+        results = self.gg.generate_combinations(3)
+        output = [[0, 1, 2], [0, 1, 3], [0, 1, 4], [0, 1, 5], [0, 2, 3],
+                  [0, 2, 4], [0, 2, 5], [0, 3, 4], [0, 3, 5], [0, 4, 5],
+                  [1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5],
+                  [1, 4, 5], [2, 3, 4], [2, 3, 5], [2, 4, 5], [3, 4, 5]]
+        np.testing.assert_equal(results, self.add_pair_matrix(output))
+
+        results = self.gg.generate_combinations(1)
+        output = [[0], [1], [2], [3], [4], [5]]
+        np.testing.assert_equal(results, self.add_pair_matrix(output))
+
+        results = self.gg.generate_combinations(0)
+        output = []
+        np.testing.assert_equal(results, self.add_pair_matrix(output))
+
+if __name__ == '__main__':
+    unittest.main()
