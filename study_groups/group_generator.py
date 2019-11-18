509a27e182d4f9629105e1f6a7b4ea08621b9fce
Melanie Simet <melanie.simet@gmail.com>
2019-11-18 15:08:20 -0600

    Lint main code files

--- study_groups/group_generator.py
+++ study_groups/group_generator.py
@@ -1,19 +1,53 @@
 """
-group_generator.py: contains the main implementation of GroupGenerator, the main object for the StudyGroups package.
+group_generator.py: contains the main implementation of GroupGenerator, the main object for the
+StudyGroups package.
 """
+#pylint: disable=C0103, R0201
 import yaml
 import numpy as np
 
 class GroupGenerator:
+    """
+    Generates a set of small groups from a list of items (such as students in a class), attempting
+    to produce a good distribution of partners/small group members for each item in the list.
+
+    This is operated by the use of a yaml configuration file.  The file should look something like
+    this:
+    ```
+    names:
+        - Annie
+        - Brian
+        - Christine
+        - David
+        - Esther
+        - Frank
+    groups:
+        - distribution: 2, 2, 2
+          num: 4
+        - distribution: 3, 3
+          num: 2
+    ```
+    That is, we have a class with six people, and we would like to divide them into pairs 4 times
+    and groups of 3 two times, for a final result of six individual groupings.
+
+    This algorithm runs in O(n^2) time and may be unfeasible for large groups.
+
+    Parameters:
+    -----------
+    yaml_file: str or path
+        A YAML config file with the format above
+    """
     def __init__(self, yaml_file):
         with open(yaml_file) as f:
             config = yaml.load(f, Loader=yaml.SafeLoader)
         if 'names' not in config or 'groups' not in config:
-            raise TypeError("YAML file {} must have a 'names' key and a 'groups' key".format(yaml_file))
+            raise TypeError(
+                "YAML file {} must have a 'names' key and a 'groups' key".format(yaml_file))
         self.n = len(config['names'])
         self.names = config['names']
         self.indices = np.arange(self.n, dtype=int)
-        # matrix: the joint appearances count (if student 1 & 2 appear together once, matrix[1,2]==matrix[2,1]==1),
+        # matrix: the joint appearances count
+        # (if student 1 & 2 appear together once, matrix[1,2]==matrix[2,1]==1).
         self.matrix = np.zeros((self.n, self.n))
         if 'seed' in config:
             np.random.seed(config['seed'])
@@ -114,20 +148,20 @@ class GroupGenerator:
         return [(ret, self.pair_matrix([ret])) for ret in return_list]
 
     def generate_divisions(self, dist):
-        # Generate all possible ways to divide our internal set of students into a distribution of groups dist.
-        # For example, if dist=[4,4,4] for 12 students, take the results of generate_combinations(4) and
-        # produce every set of 3 combinations that contains 12 unique students.
+        # Generate all possible ways to divide our internal set of students into a distribution of
+        # groups dist. For example, if dist=[4,4,4] for 12 students, take the results of
+        # generate_combinations(4) and produce every set of 3 combinations that contains 12 unique
+        # students.
         pass
-        
+
     def choose_division(self, dist):
         # Given the results of generate_divisions, select the optimal group
         pass
-        
+
     def choose_groups(self):
         # Run chose_division for every requested grouping.
         pass
-        
+
     def print_groups(self):
         # Print the results as a CSV to the terminal.
         pass
-        
--- tests/test_group_generator.py
+++ tests/test_group_generator.py
@@ -1,38 +1,44 @@
-import numpy as np
+""" test_group_generator: test the GroupGenerator() object of StudyGroups. """
+#pylint: disable=C0103,R0201,W0212
+
 import unittest
+import numpy as np
 try:
     import study_groups as sg
 except ImportError:
-#    import os
-#    os.path.append('..')
+    import sys
+    sys.path.append('..')
     import study_groups as sg
 
 class TestInitialization(unittest.TestCase):
     """ Tests that all initialization succeeds (or fails) as intended. """
     def test_init(self):
-        """ Tests that missing filenames or bad input files fail, and that correct input files are properly
-            ingested.
+        """ Tests that missing filenames or bad input files fail, and that correct input files
+            are properly ingested.
         """
         np.testing.assert_raises(TypeError, sg.GroupGenerator)
         gg = sg.GroupGenerator('test0.yaml')
         np.testing.assert_equal(gg.n, 12)
-        np.testing.assert_equal(gg.names, ['Alice', 'Bob', 'Charisma', 'Dexter', 'Emily', 'Franklin',
-                                          'Greta', 'Hamlet', 'Ivy', 'Jasper', 'Katie', 'Louis'])
+        np.testing.assert_equal(gg.names, ['Alice', 'Bob', 'Charisma', 'Dexter',
+                                           'Emily', 'Franklin', 'Greta', 'Hamlet',
+                                           'Ivy', 'Jasper', 'Katie', 'Louis'])
         np.testing.assert_equal(gg.indices, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
         np.testing.assert_equal(gg.matrix, np.zeros((12, 12)))
-        
+
         np.testing.assert_raises(TypeError, sg.GroupGenerator, 'test1.yaml')
         np.testing.assert_raises(TypeError, sg.GroupGenerator, 'test2.yaml')
         gg = sg.GroupGenerator('test3.yaml')
         np.testing.assert_equal(gg.n, 6)
-        np.testing.assert_equal(gg.names, ['Alice', 'Bob', 'Charisma', 'Dexter', 'Emily', 'Franklin'])
+        np.testing.assert_equal(gg.names, ['Alice', 'Bob', 'Charisma',
+                                           'Dexter', 'Emily', 'Franklin'])
         np.testing.assert_equal(gg.indices, [0, 1, 2, 3, 4, 5])
         np.testing.assert_equal(gg.matrix, np.zeros((6, 6)))
 
     def test_rng_seed(self):
-        """ Tests that the random number seed is actually seeding when set in a yaml file, to produce
-            replicable results.
+        """ Tests that the random number seed is actually seeding when set in a yaml file, to
+            produce replicable results.
         """
+        #pylint: disable=W0612
         gg = sg.GroupGenerator('test0.yaml')
         ri = sg.group_generator.np.random.rand()
         np.random.seed(271)
